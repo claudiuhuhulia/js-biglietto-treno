4b7f597f5c5be1bbae4a21cc595ec16bdc81ebb4
cloudhuh <claudiu.huhulia@gmail.com>
2023-05-26 18:30:18 +0200

    ticketprice calcle

--- main.js
+++ main.js
@@ -6,8 +6,28 @@ console.log('JS-OK')
     - 0.21€ al km
     - sconto 20% ai minorenni
     - sconto 40%  over 65
-
-
-
-
 */
+
+const price = document.getElementById('price');
+console.log(price);
+
+const km=prompt('Inserisci i km che intendi percorrere', 15);
+console.log(km);
+
+const age=prompt('Inserisci la tua età', 25);
+console.log(age);
+
+let travelprice=km * 0.21;
+console.log(travelprice);
+
+if(age < 18){
+    travelprice=travelprice * 80 / 100
+}
+console.log(travelprice)
+
+ if(age >= 65){
+    travelprice=travelprice * 60 / 100
+}
+console.log(travelprice)
+
+price.innerText= travelprice + "€";

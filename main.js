f2417de22e1dbedef3c6add37fe29a918f672cfd
cloudhuh <claudiu.huhulia@gmail.com>
2023-05-26 14:56:12 +0200

    ladder

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 console.log('JS-OK')
 /* 
-
+0. colleghiamo elemento dal DOM
+1. chiediamo i dati all'utente 
+2. calcola il prezzo totale del viaggio
+    - 0.21€ al km
+    - sconto 20% ai minorenni
+    - sconto 40%  over 65
 
 
 
---
+++ steps.md
@@ -0,0 +1,7 @@
+# Calcolo il prezzo del biglietto #
+
+1. Chiediamo i dati all'utente
+2. Calcoliamo il prezzo totale del viaggio 
+    - 0.21€ al km
+    - Sconto 20% ai minorenni
+    - Sconto 40% agli over 65
